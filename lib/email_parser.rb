291915c77eaba8cacb9c153de6d798867b34bda4
Sean B <butts.sean01@gmail.com>
2020-08-09 21:20:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,17 +2,3 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
-
-class Anagram
-
-  attr_accessor :name
-
-  def initialize(word)
-    @name = word 
-  end   
-
-  def match(array)
-    array.select {|x| x.split("").sort == @name.split("").sort}
-  end   
-
-end 
